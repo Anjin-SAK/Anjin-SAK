386e36b098f451108a953c2741e05b627ddc0029
Anjin-SAK <anjinkumar2129@gmail.com>
2025-08-25 00:44:22 +0530

    portfolio added

---
+++ script.js
@@ -0,0 +1,11 @@
+// Example JS
+console.log("Portfolio website loaded!");
+
+// Smooth scroll effect
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
